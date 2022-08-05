c664f43c500344236856a25c564dd8997edba092
levan-dolidze <64006772+levan-dolidze@users.noreply.github.com>
2022-08-05 18:02:15 +0300

    footer markup added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,11 +9,9 @@ const routes: Routes = [
     pathMatch: 'full'
   },
   { path: 'home', loadChildren: () => import('./components/home/home.module').then(m => m.HomeModule) },
-  { path: 'about', loadChildren: () => import('./components/about/about.module').then(m => m.AboutModule) },
   { path: 'sale', loadChildren: () => import('./components/sale/sale.module').then(m => m.SaleModule) },
   { path: 'deck', loadChildren: () => import('./components/products/deck/deck.module').then(m => m.DeckModule) },
   { path: 'wheel', loadChildren: () => import('./components/products/wheel/wheel.module').then(m => m.WheelModule) },
-  { path: 'contact', loadChildren: () => import('./components/contact/contact.module').then(m => m.ContactModule) },
   { path: 'truck', loadChildren: () => import('./components/products/truck/truck.module').then(m => m.TruckModule) },
   { path: 'complete', loadChildren: () => import('./components/products/complete/complete.module').then(m => m.CompleteModule) },
   { path: 'gallery', loadChildren: () => import('./components/gallery/gallery.module').then(m => m.GalleryModule) },
